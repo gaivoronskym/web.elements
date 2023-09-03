e9625815dbe8a3fb9491affd08bbddc1ab6ccf0c
gaivoronsky <gaivoronsky.work@gmail.com>
2023-09-03 09:31:13 -0400

    temp commit

--- CustomServer/Program.cs
+++ CustomServer/Program.cs
@@ -10,11 +10,12 @@ namespace CustomServer
         {
             await new TcpBackend(
                     new PtBranch(
-                        // new BranchRoute("/books", new PtMethod("GET", new PtBooks())),
-                        // new BranchRoute(@"/books/{bookId:\d+}/pages", new PtMethod("GET", new PtBookPages())),
-                        new BranchRoute(@"/books/{bookId:\d+}", new PtMethod("GET", new PtBook()))
-                        // new BranchRoute(@"/books/{bookId:\d+}/html", new PtMethod("GET", new PtBookHtml())),
-                        // new BranchRoute(@"/books/{bookId:\d+}/authors/{authorId:\d+}", new PtMethod("GET", new PtBookAuthors()))
+                        new BranchRoute("/books", new PtMethod("GET", new PtBooks())),
+                        new BranchRoute("/auth/login", new PtMethod("POST", new PtLogin())),
+                        new BranchRoute(@"/books/{bookId:\d+}/pages", new PtMethod("GET", new PtBookPages())),
+                        new BranchRoute(@"/books/{bookId:\d+}", new PtMethod("GET", new PtBook())),
+                        new BranchRoute(@"/books/{bookId:\d+}/html", new PtMethod("GET", new PtBookHtml())),
+                        new BranchRoute(@"/books/{bookId:\d+}/authors/{authorId:\d+}", new PtMethod("GET", new PtBookAuthors()))
                     ),
                     5436)
                 .Start();
