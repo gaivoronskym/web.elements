ef634e9d89e4c3c76cf12a33c4c47c2110253697
gaivoronskym <gaivoronsky.work@gmail.com>
2024-10-01 15:10:58 -0400

    FkWrap UP-VERSION

---
+++ Point/Fk/FkWrap.cs
@@ -0,0 +1,33 @@
+﻿using Point.Rq.Interfaces;
+using Yaapii.Atoms.Enumerable;
+
+namespace Point.Fk;
+
+public abstract class FkWrap : IFork
+{
+    private readonly IEnumerable<IFork> forks;
+
+    protected FkWrap(params IFork[] forks)
+        : this(new ManyOf<IFork>(forks))
+    {
+    }
+
+    protected FkWrap(IEnumerable<IFork> forks)
+    {
+        this.forks = forks;
+    }
+
+    public async Task<IOpt<IResponse>> Route(IRequest req)
+    {
+        foreach (var fork in this.forks)
+        {
+            var res = await fork.Route(req);
+            if (res.Has())
+            {
+                return res;
+            }
+        }
+
+        return new IOpt<IResponse>.Empty();
+    }
+}
