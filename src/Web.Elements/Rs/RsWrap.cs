1d035b79284cc18a698ee3f17a0e25ab23be7924
gaivoronskym <gaivoronsky.work@gmail.com>
2024-12-31 13:38:06 +0200

    ** Rs Xml ** Unit Test

--- src/Web.Elements/Rs/RsWrap.cs
+++ src/Web.Elements/Rs/RsWrap.cs
@@ -1,21 +1,35 @@
-﻿namespace Web.Elements.Rs;
+﻿using Yaapii.Atoms;
+using Yaapii.Atoms.Scalar;
+
+namespace Web.Elements.Rs;
 
 public abstract class RsWrap : IResponse
 {
-    private readonly IResponse origin;
+    private readonly IScalar<IResponse> origin;
+
 
     public RsWrap(IResponse origin)
+        : this(() => origin)
+    {
+    }
+
+    public RsWrap(Func<IResponse> origin)
+        : this(new ScalarOf<IResponse>(origin))
+    {
+    }
+    
+    public RsWrap(IScalar<IResponse> origin)
     {
         this.origin = origin;
     }
 
     public IEnumerable<string> Head()
     {
-        return origin.Head();
+        return origin.Value().Head();
     }
 
     public Stream Body()
     {
-        return origin.Body();
+        return origin.Value().Body();
     }
 }
---
+++ tests/Web.Elements.Tests/Rs/RsXmlTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Linq;
+using Web.Elements.Rs;
+using Yaapii.Atoms.Text;
+
+namespace Web.Elements.Tests.Rs;
+
+public class RsXmlTest
+{
+    [Fact]
+    public void MakesJsonResponse()
+    {
+        var xml = XDocument.Parse("<root><inner>Hello, world</inner></root>").ToString();
+
+        Assert.Equal(
+            new Joined(
+                Environment.NewLine,
+                "HTTP/1.1 200 OK",
+                $"Content-Length: {xml.Length}",
+                "Content-Type: application/xml",
+                "",
+                xml
+            ).AsString(),
+            new RsPrint(
+                new RsXml(
+                    xml
+                )
+            ).AsString()
+        );
+    }
+}
